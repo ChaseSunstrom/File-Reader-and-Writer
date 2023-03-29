33bb4d14722c8b4f55c77b1f4a8f5bc7f1899e50
Chase Sunstrom <Casunstrom+github@gmail.com>
2023-03-29 09:36:52 -0500

    Finished project

---
+++ FileOptins.cs
@@ -0,0 +1,90 @@
+﻿using System.Net;
+
+namespace FileReaderAndWriter;
+
+public static class FileOptins
+{
+    public static void ReadFile(string fileName)
+    {
+        if (File.Exists(fileName))
+        {
+            Console.WriteLine("Reading...");
+            Console.WriteLine(File.ReadAllText(fileName));
+        } 
+        
+        else 
+            Console.WriteLine("File does not exist, please try again."); 
+    }
+
+    public static void WriteFile(string fileName, string content)
+    {
+        if (File.Exists(fileName) && new FileInfo(fileName).Length == 0)
+        {
+            Console.WriteLine("Writing..");
+            File.WriteAllText(fileName, content);
+        }
+        
+        else if (File.Exists(fileName) && new FileInfo(fileName).Length != 0)
+            Console.WriteLine("File contains data, please delete it first.");
+        
+        else
+            Console.WriteLine("File does not exist, please try again.");
+    }
+
+    public static void DeleteFile(string fileName)
+    {
+        if (File.Exists(fileName))
+        {
+            Console.WriteLine("Deleting...");
+            File.Delete(fileName);
+        }
+        
+        else
+            Console.WriteLine("File does not exist, please try again.");
+    }
+    
+    public static void CreateFile(string fileName)
+    {
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine("Creating...");
+            File.Create(fileName);
+        }
+
+        else
+            Console.WriteLine("File already exists, please try again.");
+    }
+
+    public static void CreateAndWriteFile(string fileName, string content)
+    {
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine("Creating...");
+            File.Create(fileName);
+            Console.WriteLine("Writing...");
+            File.WriteAllText(fileName, content);
+            Console.WriteLine("Done!");
+            Console.WriteLine("New file content: \n" + File.ReadAllText(fileName));
+        }
+        
+        else
+            Console.WriteLine("File already exists, please try again.");
+    }
+
+    public static void ReadAndWriteFile(string fileName, string content)
+    {
+        if (File.Exists(fileName))
+        {
+            Console.WriteLine("Reading...");
+            Console.WriteLine(File.ReadAllText(fileName));
+            string text = File.ReadAllText(fileName);
+            Console.WriteLine("Writing...");
+            fileName.Replace(text, content);
+            Console.WriteLine("New file content:\n" + File.ReadAllText(fileName));
+            Console.WriteLine();
+        }
+        
+        else
+            Console.WriteLine("File does not exist, please try again.");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace FileReaderAndWriter
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool exit = false;
+            string fileName;
+            string content;
+            
+            while (!exit)
+            {
+                Console.WriteLine("Enter a number:\n1. Read from file\n2. Write to file\n3. Create a file\n4. Delete a file\n5. Create and Write to file\n6. Read and write to file\n7. Exit");
+                string answer = Console.ReadLine();
+
+                try
+                {
+                    switch (answer)
+                    {
+                        case "1":
+                            Console.WriteLine("Enter the file name:");
+                            fileName = Console.ReadLine();
+                            FileOptins.ReadFile(fileName);
+                            break;
+                        case "2":
+                            Console.WriteLine("Enter the file name:");
+                            fileName = Console.ReadLine();
+                            Console.WriteLine("Enter the file contents:");
+                            content = Console.ReadLine();
+                            FileOptins.WriteFile(fileName, content);
+                            break;
+                        case "3":
+                            Console.WriteLine("Enter the file name:");
+                            fileName = Console.ReadLine();
+                            FileOptins.CreateFile(fileName);
+                            break;
+                        case "4":
+                            Console.WriteLine("Enter the file name:");
+                            fileName = Console.ReadLine();
+                            FileOptins.DeleteFile(fileName);
+                            break;
+                        case "5":
+                            Console.WriteLine("Enter the file name:");
+                            fileName = Console.ReadLine();
+                            Console.WriteLine("Enter the file contents:");
+                            content = Console.ReadLine();
+                            FileOptins.CreateAndWriteFile(fileName, content);
+                            break;
+                        case "6":
+                            Console.WriteLine("Enter the file name:");
+                            fileName = Console.ReadLine();
+                            Console.WriteLine("Enter the file contents:");
+                            content = Console.ReadLine();
+                            FileOptins.ReadAndWriteFile(fileName, content);
+                            break;
+                        case "7":
+                            exit = true;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid input, please try again.");
+                            break;
+                    }
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+        }
+    }
+}
